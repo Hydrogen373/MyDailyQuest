7294043c6fbbb308cd585e40db718241459afa08
Hydrogen373 <shyun2314@naver.com>
2023-07-04 22:01:42 +0900

    [CHORE] GUI card layout test

--- MyDailyQuest/src/dqgui/GUIManager.java
+++ MyDailyQuest/src/dqgui/GUIManager.java
@@ -1,31 +1,49 @@
 package dqgui;
 
+import java.awt.CardLayout;
+import java.awt.FlowLayout;
+import java.util.ArrayList;
+
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SpringLayout;
 
 public class GUIManager {
 	
 	public void test() {
 		JFrame mainFrame = new JFrame();
-		
-		JPanel panel = new JPanel();
+		JPanel mainPanel = new JPanel();
+		CardLayout cards = new CardLayout();
+		mainPanel.setLayout(cards);
 
+		mainPanel.add(new JLabel("Test GUI. Hello world!!"));
 		
-		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-		panel.add(new JLabel("Test GUI. Hello world!!"));
-		TaskBox task0 = new TaskBox("00", "Study English", "20220101", false, null);
-		panel.add(task0.getPanel());
+		ArrayList<TaskBox> tasks = new ArrayList<TaskBox>();
+		tasks.add(new TaskBox("00", "Study English", "20220101", false, null));
+		tasks.add(new TaskBox("01", "Study Korean", "20220101", false, null));
+		tasks.add(new TaskBox("02", "Study Japanesh", "20220101", false, null));
+
+		JPanel tasksPanel = new JPanel();
+		tasksPanel.setLayout(new BoxLayout(tasksPanel, BoxLayout.PAGE_AXIS));
+		for (TaskBox task : tasks) {
+			tasksPanel.add(task.getPanel());
+		}
 		
-		mainFrame.add(panel);
+		mainPanel.add(tasksPanel);
 		
 		
+		mainFrame.add(mainPanel);
 		mainFrame.setVisible(true);
 		mainFrame.setSize(840, 700);
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainFrame.setLocationRelativeTo(null);
 //		mainFrame.setResizable(false);
+		
+		tasks.get(0).modifyContent("this is new content");
+		
+		cards.next(mainPanel);
 		
 	}
 	
--- MyDailyQuest/src/dqgui/TaskBox.java
+++ MyDailyQuest/src/dqgui/TaskBox.java
@@ -1,13 +1,16 @@
 package dqgui;
 
+import java.awt.Color;
+
 import javax.swing.BoxLayout;
+import javax.swing.GroupLayout.Alignment;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class TaskBox {
 	String uid ="";
-	JLabel content = new JLabel();
+//	JLabel content = new JLabel();
 	JLabel recent_completion_date = new JLabel();
 	String tmp_completion_date;
 	boolean done = false;
@@ -17,16 +20,22 @@ public class TaskBox {
 	
 	public TaskBox(String uid, String content, String recent_completion_date, boolean done, String tmp_completion_date) {
 		this.uid = uid;
-		this.content.setText(content);
+//		this.content.setText(content);
 		this.recent_completion_date.setText(recent_completion_date);
 		this.done = done;
 		check.setSelected(done);
 		this.tmp_completion_date = tmp_completion_date;
 		
 		panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
-		panel.add(check);
+		panel.setAlignmentX(0);
 		panel.add(this.recent_completion_date);
-		panel.add(this.content);
+		panel.add(check);
+		check.setText(content);
+		
+		//debug
+		panel.add(new JLabel("debug content"));
+		panel.setBackground(Color.green);
+
 		
 	}
 	
@@ -35,7 +44,7 @@ public class TaskBox {
 	}
 	
 	public void modifyContent(String newContent) {
-		this.content.setText(newContent);
+		this.check.setText(newContent);
 	}
 	
 	
